c769cb73f78a486f8d992ff7056db342694c2b49
Matthew Turk <matthewturk@gmail.com>
2021-02-13 09:11:25 -0600

    Make cacheSeed not fail

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
+var webpack = require("webpack");
 module.exports = {
     experiments: {
         topLevelAwait: true,
         asyncWebAssembly: true,
-    }
-};
+    },
+     plugins: [
+         // This is just a way to make this not happen.  From what I can tell,
+         // emulators.cacheSeed is modifying the value on an interface, since
+         // it doesn't touch the emulators-impl.  I'm sure there's a better way
+         // to do this.  But I can't seem to figure it out.
+         new webpack.DefinePlugin({
+             'emulators.cacheSeed': webpack.DefinePlugin.runtimeValue(() => "window.title")
+         }),
+     ]
+}

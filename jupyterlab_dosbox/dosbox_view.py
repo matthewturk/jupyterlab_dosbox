9fc42527d175100e2801047e0b9700604626eee9
Matthew Turk <matthewturk@gmail.com>
2021-02-17 15:57:36 -0600

    Adding screenshotting

--- jupyterlab_dosbox/dosbox_view.py
+++ jupyterlab_dosbox/dosbox_view.py
@@ -1,6 +1,8 @@
 from ._version import __version__
 import ipywidgets
 import traitlets
+from ipywidgets.widgets.trait_types import bytes_serialization
+import numpy as np
 
 EXTENSION_VERSION = __version__
 
@@ -13,8 +15,17 @@ class DosboxModel(ipywidgets.DOMWidget):
     _view_module = traitlets.Unicode('jupyterlab-dosbox').tag(sync=True)
     _view_module_version = traitlets.Unicode(EXTENSION_VERSION).tag(sync=True)
     activelayer = traitlets.Unicode('default').tag(sync=True)
+    _last_screenshot = traitlets.Bytes(allow_none=True).tag(sync=True, **bytes_serialization)
 
     def send_line(self, line):
         command = {'name': 'sendKeys', 'args': ["KBD_%s" % _.lower() for _ in
         line]}
         self.send(command)
+
+    def screenshot(self):
+        self.send({'name': 'screenshot', 'args': []})
+    
+    @property
+    def last_screenshot(self):
+        if self._last_screenshot is None or len(self._last_screenshot) == 0: return None
+        return np.frombuffer(self._last_screenshot, dtype="u1").reshape((400,640, 4))

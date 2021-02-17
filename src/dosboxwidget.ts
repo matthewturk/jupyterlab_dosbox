718bfc9c2bb18e853933c941239121b1005310ba
Matthew Turk <matthewturk@gmail.com>
2021-02-17 06:20:42 -0600

    moving late binding to the model

--- src/dosboxwidget.ts
+++ src/dosboxwidget.ts
@@ -1,12 +1,18 @@
 import { Widget } from '@lumino/widgets';
-import { DOMWidgetModel, ISerializers, WidgetModel, unpack_models } from '@jupyter-widgets/base';
+import {
+  DOMWidgetModel,
+  ISerializers,
+  WidgetModel,
+  unpack_models
+} from '@jupyter-widgets/base';
 import { MODULE_NAME, MODULE_VERSION } from './version';
 
 import { CommandInterface, Emulators } from 'emulators';
-import { DosFactoryType, DosInstance } from 'emulators-ui/dist/types/js-dos';
+import { DosFactoryType, DosInstance, DosOptions } from 'emulators-ui/dist/types/js-dos';
 
 import { URLExt } from '@jupyterlab/coreutils';
 import { ServerConnection } from '@jupyterlab/services';
+import { EmulatorsUi } from 'emulators-ui';
 //import { EmulatorsUi } from 'emulators-ui';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -14,11 +20,15 @@ const _emulators = await import('emulators');
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const _emulatorsUi = await import('emulators-ui');
 
+// Having this be 'janus' causes some issues we can't work around
+const workerType = 'dosWorker';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 //const _wworker = await import('emulators/dist/wworker');
 
 declare const Dos: DosFactoryType;
 declare const emulators: Emulators;
+declare const emulatorsUi: EmulatorsUi;
 
 export class DosboxWidget extends Widget {
   constructor() {
@@ -62,12 +72,6 @@ export class DosboxWidget extends Widget {
   }
 }
 
-export class LateBindingEmulatorsUi extends EmulatorsUi {
-      dos: DosFactoryType = (root: HTMLDivElement, options?: DosOptions) => {
-        return new DosInstance(root, this, options || {});
-    };
-}
-
 export class DosboxRuntimeModel extends DOMWidgetModel {
   defaults(): any {
     return {
@@ -78,8 +82,9 @@ export class DosboxRuntimeModel extends DOMWidgetModel {
     };
   }
 
-  initialize(attributes: any, options: any): void {
+  async initialize(attributes: any, options: any): Promise<void> {
     super.initialize(attributes, options);
+    this.emulatorsUi = emulatorsUi;
     const settings = ServerConnection.makeSettings();
     const requestUrl = URLExt.join(
       settings.baseUrl,
@@ -87,11 +92,8 @@ export class DosboxRuntimeModel extends DOMWidgetModel {
       'bundles',
       'a1.jsdos'
     );
-    this.dos = Dos(this.dosDiv, { emulatorFunction: 'dosWorker' });
-    this.ci = await this.dos.run(requestUrl);
+    this.ci = await this.run(requestUrl);
     this.ci.screenshot();
-  }
-
   }
 
   static serializers: ISerializers = {
@@ -99,12 +101,71 @@ export class DosboxRuntimeModel extends DOMWidgetModel {
     lastScreenshot: { serialize: serializeArray, deserialize: deserializeArray }
   }
 
+  async run(
+    bundleUrl: string,
+    optionalChangesUrl?: string
+  ): Promise<CommandInterface> {
+    await this.stop();
+    const changesUrl = optionalChangesUrl || bundleUrl + '.changed';
+    const emulatorsUi = this.emulatorsUi;
+    //this.layers.setLoadingMessage('Downloading bundle ...');
+    const bundlePromise = emulatorsUi.network.resolveBundle(bundleUrl, {
+      onprogress: (percent: number) =>
+        // This should be replaced with a jupyter specific loading message, or something
+        //this.layers.setLoadingMessage('Downloading bundle ' + percent + '%')
+        console.log('Downloading bundle ' + percent + '%')
+    });
+    try {
+      const changesBundle = await emulatorsUi.persist
+        .load(changesUrl, emulators)
+        .catch(() =>
+          emulatorsUi.network.resolveBundle(changesUrl, { cache: null })
+        );
+      const bundle = await bundlePromise;
+      this.ciPromise = emulators[workerType]([bundle, changesBundle]);
+    } catch {
+      const bundle = await bundlePromise;
+      this.ciPromise = emulators[workerType]([bundle]);
+    }
+
+    let ci: CommandInterface;
+    try {
+      //this.layers.setLoadingMessage('Starting...');
+      ci = await this.ciPromise;
+    } catch (e) {
+      console.log('Unexpected error occurred');
+      //this.layers.setLoadingMessage('Unexpected error occured...');
+      //this.layers.notyf.error({
+      //message: "Can't start emulator look browser logs for more info"
+      //});
+      console.error(e);
+      throw e;
+    }
+
+    return ci;
+  }
+
+  async stop(): Promise<void> {
+    //this.layers.showLoadingLayer();
+
+    if (this.ciPromise === undefined) {
+      return;
+    }
+
+    const ci = await this.ciPromise;
+    delete this.ciPromise;
+    await ci.exit();
+
+    return;
+  }
+
   dos: DosInstance;
   ci: CommandInterface;
   lastScreenshot: Uint8Array;
+  emulatorsUi: EmulatorsUi;
+  ciPromise?: Promise<CommandInterface>;
 
   static model_name = 'DosboxRuntimeModel';
   static model_module = MODULE_NAME;
   static model_module_version = MODULE_VERSION;
-
 }

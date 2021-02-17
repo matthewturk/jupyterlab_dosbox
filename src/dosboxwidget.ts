474fa51dc9227b2871d50491ff854e6fc7822b30
Matthew Turk <matthewturk@gmail.com>
2021-02-17 05:38:05 -0600

    halfway to sketching out widgets

--- src/dosboxwidget.ts
+++ src/dosboxwidget.ts
@@ -1,4 +1,6 @@
 import { Widget } from '@lumino/widgets';
+import { DOMWidgetModel, ISerializers, WidgetModel, unpack_models } from '@jupyter-widgets/base';
+import { MODULE_NAME, MODULE_VERSION } from './version';
 
 import { CommandInterface, Emulators } from 'emulators';
 import { DosFactoryType, DosInstance } from 'emulators-ui/dist/types/js-dos';
@@ -58,4 +60,51 @@ export class DosboxWidget extends Widget {
     console.log('Creating a new Dos instance');
     this.startDos();
   }
+}
+
+export class LateBindingEmulatorsUi extends EmulatorsUi {
+      dos: DosFactoryType = (root: HTMLDivElement, options?: DosOptions) => {
+        return new DosInstance(root, this, options || {});
+    };
+}
+
+export class DosboxRuntimeModel extends DOMWidgetModel {
+  defaults(): any {
+    return {
+      ...super.defaults(),
+      _model_name: DosboxRuntimeModel.model_name,
+      _model_module: DosboxRuntimeModel.model_module,
+      _model_module_version: DosboxRuntimeModel.model_module_version
+    };
+  }
+
+  initialize(attributes: any, options: any): void {
+    super.initialize(attributes, options);
+    const settings = ServerConnection.makeSettings();
+    const requestUrl = URLExt.join(
+      settings.baseUrl,
+      'jupyterlab_dosbox', // API Namespace
+      'bundles',
+      'a1.jsdos'
+    );
+    this.dos = Dos(this.dosDiv, { emulatorFunction: 'dosWorker' });
+    this.ci = await this.dos.run(requestUrl);
+    this.ci.screenshot();
+  }
+
+  }
+
+  static serializers: ISerializers = {
+    ...DOMWidgetModel.serializers,
+    lastScreenshot: { serialize: serializeArray, deserialize: deserializeArray }
+  }
+
+  dos: DosInstance;
+  ci: CommandInterface;
+  lastScreenshot: Uint8Array;
+
+  static model_name = 'DosboxRuntimeModel';
+  static model_module = MODULE_NAME;
+  static model_module_version = MODULE_VERSION;
+
 }

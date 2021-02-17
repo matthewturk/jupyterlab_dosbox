244ee0a46a1bca52a6f1163ffa6f409e31d925ea
Matthew Turk <matthewturk@gmail.com>
2021-02-16 20:05:48 -0600

    Use a URL handler to manage wasm

--- jupyterlab_dosbox/handlers.py
+++ jupyterlab_dosbox/handlers.py
@@ -3,6 +3,8 @@ import json
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
+import pkg_resources
+import os
 
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
@@ -11,32 +13,35 @@ class RouteHandler(APIHandler):
     @tornado.web.authenticated
     def get(self):
         # This needs to be implemented
-        self.finish(open("a1.jsdos", "rb").read())
+        self.finish(pkg_resources.resource_stream("jupyterlab_dosbox",
+                os.path.join("bundles", "a1.jsdos")).read())
 
 class RouteHandlerChanged(APIHandler):
     @tornado.web.authenticated
     def get(self):
         # This needs to be implemented
-        self.finish(open("a1.jsdos.changed", "rb").read())
+        self.finish(pkg_resources.resource_stream("jupyterlab_dosbox",
+                os.path.join("bundles", "a1.jsdos.changed")).read())
 
 class RouteWasmHandler(APIHandler):
     @tornado.web.authenticated
-    def get(self, module):
-        # This needs to be implemented
-        raise RuntimeError
+    def get(self, mod, extension):
+        self.finish(pkg_resources.resource_stream("jupyterlab_dosbox",
+                os.path.join("bundles", mod + "." + extension)).read())
+
 
 def setup_handlers(web_app):
     host_pattern = ".*$"
 
     base_url = web_app.settings["base_url"]
-    route_pattern = url_path_join(base_url, "jupyterlab_dosbox", "get_example", "a1.jsdos")
+    route_pattern = url_path_join(base_url, "jupyterlab_dosbox", "bundles", "a1.jsdos")
     handlers = [(route_pattern, RouteHandler)]
     web_app.add_handlers(host_pattern, handlers)
     # This is embarassing!  Why have two handlers when we could do with one?!?
-    route_pattern_changed = url_path_join(base_url, "jupyterlab_dosbox", "get_example", "a1.jsdos.changed")
+    route_pattern_changed = url_path_join(base_url, "jupyterlab_dosbox", "bundles", "a1.jsdos.changed")
     handlers_changed = [(route_pattern_changed, RouteHandlerChanged)]
     web_app.add_handlers(host_pattern, handlers_changed)
 
-    route_wasm_modules = url_path_join(base_url, "jupyterlab_dosbox", "wasm", "(.*)")
+    route_wasm_modules = url_path_join(base_url, "jupyterlab_dosbox", "wasm", "(.*).(js|wasm)")
     handlers_wasm = [(route_wasm_modules, RouteWasmHandler)]
     web_app.add_handlers(host_pattern, handlers_wasm)
--- src/dosboxwidget.ts
+++ src/dosboxwidget.ts
@@ -1,11 +1,11 @@
 import { Widget } from '@lumino/widgets';
 
-import { CommandInterface } from 'emulators';
+import { CommandInterface, Emulators } from 'emulators';
 import { DosFactoryType, DosInstance } from 'emulators-ui/dist/types/js-dos';
 
 import { URLExt } from '@jupyterlab/coreutils';
 import { ServerConnection } from '@jupyterlab/services';
-import { EmulatorsUi } from 'emulators-ui';
+//import { EmulatorsUi } from 'emulators-ui';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const _emulators = await import('emulators');
@@ -16,11 +16,13 @@ const _emulatorsUi = await import('emulators-ui');
 //const _wworker = await import('emulators/dist/wworker');
 
 declare const Dos: DosFactoryType;
+declare const emulators: Emulators;
 
 export class DosboxWidget extends Widget {
   constructor() {
     super();
     this.dosInitialized = false;
+    emulators.pathPrefix = '/jupyterlab_dosbox/wasm/';
 
     this.addClass('dosbox-widget');
 
@@ -35,7 +37,7 @@ export class DosboxWidget extends Widget {
     const requestUrl = URLExt.join(
       settings.baseUrl,
       'jupyterlab_dosbox', // API Namespace
-      'get_example',
+      'bundles',
       'a1.jsdos'
     );
     this.dos = Dos(this.dosDiv, { emulatorFunction: 'dosWorker' });

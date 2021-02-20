f86d79d3271694e8a806d34cad19ed1bb24783e0
Matthew Turk <matthewturk@gmail.com>
2021-02-19 18:29:11 -0600

    Cache some bundles and add a directory

--- jupyterlab_dosbox/handlers.py
+++ jupyterlab_dosbox/handlers.py
@@ -10,30 +10,40 @@ import zipfile
 
 _default_components = ['dosbox.conf', 'jsdos.json']
 
-_b = io.BytesIO()
-with zipfile.ZipFile(_b, "w") as f:
-    print("Handling the zip!")
-    for fn in _default_components:
-        data = pkg_resources.resource_stream("jupyterlab_dosbox",
-                os.path.join("bundles", fn)).read()
-        f.writestr(os.path.join(".jsdos", fn), data)
-_b.seek(0)
-DEFAULT_ZIP = _b.read()
+DEFAULT_ZIP = {}
+
+def _generate_zip(bundle_name = "bundle"):
+    if bundle_name in DEFAULT_ZIP:
+        return DEFAULT_ZIP[bundle_name]
+    _b = io.BytesIO()
+    with zipfile.ZipFile(_b, "w") as f:
+        f.writestr(".jsdos/", "")
+        for fn in _default_components:
+            data = pkg_resources.resource_stream("jupyterlab_dosbox",
+                    os.path.join("bundles", fn)).read()
+            f.writestr(os.path.join(".jsdos", fn), data)
+    _b.seek(0)
+    DEFAULT_ZIP[bundle_name] = _b.read()
+    return DEFAULT_ZIP[bundle_name]
+
+if __name__ == "__main__":
+    with open("null_bundle.zip", "wb") as f:
+        f.write(_generate_zip())
 
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
     @tornado.web.authenticated
-    def get(self):
+    def get(self, *args, **kwargs):
         # This needs to be implemented
-        self.finish(DEFAULT_ZIP)
+        self.finish(_generate_zip())
 
 class RouteHandlerChanged(APIHandler):
     @tornado.web.authenticated
-    def get(self):
+    def get(self, suffix):
         # This needs to be implemented
-        self.finish(DEFAULT_ZIP)
+        self.finish(_generate_zip())
 
 class RouteWasmHandler(APIHandler):
     @tornado.web.authenticated
@@ -46,13 +56,9 @@ def setup_handlers(web_app):
     host_pattern = ".*$"
 
     base_url = web_app.settings["base_url"]
-    route_pattern = url_path_join(base_url, "jupyterlab_dosbox", "bundles", "a1.jsdos")
+    route_pattern = url_path_join(base_url, "jupyterlab_dosbox", "bundles", "a1.jsdos(\.changed)?")
     handlers = [(route_pattern, RouteHandler)]
     web_app.add_handlers(host_pattern, handlers)
-    # This is embarassing!  Why have two handlers when we could do with one?!?
-    route_pattern_changed = url_path_join(base_url, "jupyterlab_dosbox", "bundles", "a1.jsdos.changed")
-    handlers_changed = [(route_pattern_changed, RouteHandlerChanged)]
-    web_app.add_handlers(host_pattern, handlers_changed)
 
     route_wasm_modules = url_path_join(base_url, "jupyterlab_dosbox", "wasm", "(.*).(js|wasm)")
     handlers_wasm = [(route_wasm_modules, RouteWasmHandler)]

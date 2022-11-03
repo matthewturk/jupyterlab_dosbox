fba65efd35ed3ad7b1b26101fff59af6ca445935
Matthew Turk <matthewturk@gmail.com>
2022-11-03 05:03:08 -0500

    Attempting to update deps

--- setup.py
+++ setup.py
@@ -2,15 +2,9 @@
 jupyterlab_dosbox setup
 """
 import json
+import sys
 from pathlib import Path
 
-from jupyter_packaging import (
-    create_cmdclass,
-    install_npm,
-    ensure_targets,
-    combine_commands,
-    skip_if_exists
-)
 import setuptools
 
 HERE = Path(__file__).parent.resolve()
@@ -18,41 +12,32 @@ HERE = Path(__file__).parent.resolve()
 # The name of the project
 name = "jupyterlab_dosbox"
 
-lab_path = (HERE / name / "labextension")
+lab_path = HERE / name.replace("-", "_") / "labextension"
 
 # Representative files that should exist after a successful build
-jstargets = [
-    str(lab_path / "package.json"),
-]
-
-package_data_spec = {
-    name: ["*"],
-}
+ensured_targets = [str(lab_path / "package.json"), str(lab_path / "static/style.js")]
 
 labext_name = "jupyterlab-dosbox"
 
 data_files_spec = [
-    ("share/jupyter/labextensions/%s" % labext_name, str(lab_path), "**"),
-    ("share/jupyter/labextensions/%s" % labext_name, str(HERE), "install.json"),
-    ("etc/jupyter/jupyter_server_config.d",
-     "jupyter-config", "jupyterlab_dosbox.json"),
+    (
+        f"share/jupyter/labextensions/{labext_name}",
+        str(lab_path.relative_to(HERE)),
+        "**",
+    ),
+    (f"share/jupyter/labextensions/{labext_name}", str("."), "install.json"),
+    (
+        "etc/jupyter/jupyter_server_config.d",
+        "jupyter-config/server-config",
+        "jupterlab_nodeeditor.json",
+    ),
+    # For backward compatibility with notebook server
+    (
+        "etc/jupyter/jupyter_notebook_config.d",
+        "jupyter-config/nb-config",
+        "jupterlab_nodeeditor.json",
+    ),
 ]
-
-cmdclass = create_cmdclass("jsdeps",
-    package_data_spec=package_data_spec,
-    data_files_spec=data_files_spec
-)
-
-js_command = combine_commands(
-    install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
-    ensure_targets(jstargets),
-)
-
-is_repo = (HERE / ".git").exists()
-if is_repo:
-    cmdclass["jsdeps"] = js_command
-else:
-    cmdclass["jsdeps"] = skip_if_exists(jstargets, js_command)
 
 long_description = (HERE / "README.md").read_text()
 
@@ -63,23 +48,14 @@ setup_args = dict(
     name=name,
     version=pkg_json["version"],
     url=pkg_json["homepage"],
-    author=pkg_json["author"],
+    author=pkg_json["author"]["name"],
+    author_email=pkg_json["author"]["email"],
     description=pkg_json["description"],
     license=pkg_json["license"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    cmdclass=cmdclass,
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "jupyterlab~=3.0",
-        "pooch>=1.3.0",
-        "numpy>=1.13",
-        "tornado>=6.0",
-        "ipywidgets>=7.0.0"
-    ],
     zip_safe=False,
     include_package_data=True,
-    python_requires=">=3.6",
     platforms="Linux, Mac OS X, Windows",
     keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
     classifiers=[
@@ -94,6 +70,25 @@ setup_args = dict(
     ],
 )
 
+try:
+    from jupyter_packaging import get_data_files, npm_builder, wrap_installers
+
+    post_develop = npm_builder(
+        build_cmd="install:extension", source_dir="src", build_dir=lab_path
+    )
+    setup_args["cmdclass"] = wrap_installers(
+        post_develop=post_develop, ensured_targets=ensured_targets
+    )
+    setup_args["data_files"] = get_data_files(data_files_spec)
+except ImportError as e:
+    import logging
+
+    logging.basicConfig(format="%(levelname)s: %(message)s")
+    logging.warning(
+        "Build tool `jupyter-packaging` is missing. Install it with pip or conda."
+    )
+    if not ("--name" in sys.argv or "--version" in sys.argv):
+        raise e
 
 if __name__ == "__main__":
     setuptools.setup(**setup_args)
